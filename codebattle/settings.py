e9a23bf94dee6e723bb70ea7b2828c15b31f6148
sxsm <thesgadyal@gmail.com>
2022-11-16 02:29:54 +0530

    settings -n

--- codebattle/settings.py
+++ codebattle/settings.py
@@ -137,3 +137,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-b44b.up.railway.app/'
+]

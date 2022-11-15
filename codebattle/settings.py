cbe472fe0940a6b01a5a3a78efa9d0bbae7126ef
sxsm <thesgadyal@gmail.com>
2022-11-16 03:30:01 +0530

    -

--- codebattle/settings.py
+++ codebattle/settings.py
@@ -44,16 +44,17 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsPostCsrfMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    
 ]    
 
 ROOT_URLCONF = 'codebattle.urls'
